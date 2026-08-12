5a874c06615ecafcfda250fcb2540ddfc4c27446
lolichno <cheremisenov.leha@gmail.com>
2026-08-12 18:28:52 +0300

    refactor: extract interpretations

--- TheParser/Runtime/Interpreter.cs
+++ TheParser/Runtime/Interpreter.cs
@@ -4,39 +4,6 @@ using TheParser.Lexing;
 
 namespace TheParser.Runtime;
 
-public interface IStringInterpretable
-{
-    StringInterpretation InterpretToString();
-}
-
-public abstract record class Interpretation;
-
-public record class NothingInterpretation : Interpretation, IStringInterpretable
-{
-    public StringInterpretation InterpretToString()
-    {
-        return new StringInterpretation("Nothing.");
-    }
-}
-
-public record class StringInterpretation(string Value) : Interpretation, IStringInterpretable
-{
-    public StringInterpretation InterpretToString()
-    {
-        return new StringInterpretation(Value);
-    }
-}
-
-public record class NumberInterpretation(double Value) : Interpretation, IStringInterpretable
-{
-    public StringInterpretation InterpretToString()
-    {
-        return new StringInterpretation(Value.ToString());
-    }
-}
-
-public record class NullInterpretation : Interpretation;
-
 public class Interpreter
 {
     private Dictionary<string, Interpretation> _variables = new();
