1e1cd57820ac02caf72f7bf9e5ccef41e85e1db9
lolichno <cheremisenov.leha@gmail.com>
2026-09-01 00:08:21 +0300

    chore: remove debug argument from runcommand

--- TheParser/Cli/Commands/RunCommand.cs
+++ TheParser/Cli/Commands/RunCommand.cs
@@ -12,7 +12,6 @@ namespace TheParser.Cli.Commands;
 [CommandName("run")]
 [CommandResource(Resource.RunDescription)]
 [RequirePositionals("path")]
-[SupportedFlags("--debug")]
 public sealed class RunCommand(DependencyInjector injector) : CliCommand
 {
     public override CliCommandResult Run(ArgumentsProvider provider)
@@ -24,42 +23,9 @@ public sealed class RunCommand(DependencyInjector injector) : CliCommand
             return IncorrectUsage($"File '{filePath}' does not exist.");
         }
 
-        bool debug = provider.HasArgument("--debug");
-
         string content = File.ReadAllText(filePath);
 
         Lexer lexer = new(content);
-
-        if (debug)
-        {
-            Token token;
-            try
-            {
-                token = lexer.NextToken();
-            }
-            catch (LanguageException ex)
-            {
-                return CliCommandResult.Fail(ex, DebugUtility.BuildFailMessage(ex, content));
-            }
-            Console.WriteLine("Lexer:");
-            while (token.TokenType != TokenType.EOF)
-            {
-                if (token.Value != null)
-                    Console.Write($"{token.TokenType}({token.Value}) ");
-                else
-                    Console.Write($"{token.TokenType} ");
-                try
-                {
-                    token = lexer.NextToken();
-                }
-                catch (LanguageException ex)
-                {
-                    return CliCommandResult.Fail(ex, DebugUtility.BuildFailMessage(ex, content));
-                }
-            }
-
-            Console.Write(token.TokenType);
-        }
 
         lexer.Reset();
 
@@ -74,17 +40,6 @@ public sealed class RunCommand(DependencyInjector injector) : CliCommand
         catch (LanguageException ex)
         {
             return CliCommandResult.Fail(ex, DebugUtility.BuildFailMessage(ex, content));
-        }
-
-        if (debug)
-        {
-            Console.WriteLine("\nAst builder: ");
-
-            var printer = new AstPrinter();
-
-            string tree = printer.Print(statement);
-            Console.WriteLine(tree);
-            Console.WriteLine("Interpreter: ");
         }
 
         Interpreter interpreter = new(injector);
